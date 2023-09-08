c0666abf46119909d024c96b08d1bb8b0746dd25
charitha5I3 <144417558+charitha5I3@users.noreply.github.com>
2023-09-08 22:15:01 +0530

    Add files via upload

--- NestedIfExample.java
+++ NestedIfExample.java
@@ -1,31 +1,39 @@
 import java.io.*;
 import java.util.*;
-public class NestedIfExample{
-public static void main(String[] args){
-int m1,m2,m3;
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter marks of three subjects");
-m1=sc.nextInt();
-m2=sc.nextInt();
-m3=sc.nextInt();
-int total=m1+m2+m3;
-float avg=(float)total/3;
-if(avg>=75){
-if(avg>=95){
-  System.out.println("Your average is in top 5%");
+public class NestedIfExample
+{
+   public static void main(String[] args)
+   {
+     System.out.println("Enter marks of three subjects");
+     Scanner sc=new Scanner(System.in);
+     int m1=sc.nextInt();
+     int m2=sc.nextInt();
+     int m3=sc.nextInt();
+     int total=m1+m2+m3;
+     float Avg=(float)total/3;
+     if(Avg>=75)
+     {
+        if(Avg>=95)
+        {
+           System.out.println("your average is in top 5 percent");
+        }
+        else if(Avg>=90)
+        {
+           System.out.println("your average is in top 10 percent");
+        }
+        else
+        {
+           System.out.println("student passed in distinction");
+        }
+     }
+     else if(Avg>=40 && Avg<75)
+     {
+        System.out.println("student passed");
+     }
+     else
+     {
+       System.out.println("student failed");
+     }
+   }
 }
-else if(avg>=90){
-  System.out.println("Your average is in top 10%");
-}
-else{
-  System.out.println("Student passed in distinction");
-}
-}
-else if(avg>=40&&avg<75){
-  System.out.println("Student passed");
-}
-else{
-  System.out.println("Student failed");
-}
-}
-} 
+   
